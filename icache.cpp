88370fb2ad770547e91b7ac52e1737cdd79d93ec
Maddie D <madeleinedreher@utexas.edu>
2025-06-08 02:09:22 -0500

    starting a configurable icache

--- icache.cpp
+++ icache.cpp
@@ -1,16 +1,43 @@
 //address mapping: TTT TTT TII IBO OOO
 #include "config.h"
+#include <cmath>
+#include <optional> 
+
 class icache{
-struct I$_TagStoreEntry_Struct{
-    int valid_way0;
-    int tag_way0;
-    int dirty_way0;
-    int valid_way1;
-    int tag_way1;
-    int dirty_way1;
-    int lru; 
+struct cacheline {
+    char data[cache_line_size];
+    bool valid;
+    bool dirty;
+    int tag;
 };
 
-int icache_datastore[icache_banks][icache_sets][icache_ways][cache_line_size];
-int icache_datastore[icache_banks][icache_sets][icache_ways][cache_line_size];
+ cacheline cache[icache_banks][icache_sets][icache_ways];
+ char lru[icache_sets]; //stores the least recently used way for each set
+
+ icache(){
+    for(int i = 0; i < icache_banks; i++){
+        for(int j = 0; i < icache_sets; j++){
+           for(int k = 0; i < icache_ways; k++){
+            cache[i][j][k].valid = 0; //all start invalid
+           }
+        }
+    }
+ }
+cacheline icache_access(int addr){
+    int bank_mask = (1 << icache_banks) - 1;
+    int set_mask = (1 << icache_sets) - 1;
+
+    int bank = (addr >> (std::log2(icache_banks))) & bank_mask;
+    int set = (addr >> (std::log2(icache_sets))) & set_mask;
+
+    int tag = (addr >> 8) & 0x007F; //not sure if this can change
+    for(int i = 0; i < icache_ways; i++){
+        if((tag == cache[bank][set][i].tag) && cache[bank][set][i].valid){
+            return cache[bank][set][i]; //cache hit
+        }
+    }
+    return std::nullopt;   //cache miss
+}
+
+
 };
