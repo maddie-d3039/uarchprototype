8770b7e3f0e97a478c1241c0a17a45ca042085cc
Maddie D <madeleinedreher@utexas.edu>
2025-06-12 13:14:33 -0500

    added tlb.cpp and changed icache.cpp

--- icache.cpp
+++ icache.cpp
@@ -3,6 +3,23 @@
 #include <cmath>
 #include <optional> 
 
+enum origin{
+    icache,
+    dcache,
+    TLB,
+}
+
+enum class miss_tpye {
+    none,      
+    tlb_miss,
+    tag_miss
+};
+
+struct cache_lookup_result {
+    cacheline line;     // valid only if hit == true
+    MissType miss_reason; // meaningful only if hit == false
+};
+
 class icache{
 struct cacheline {
     char data[cache_line_size];
@@ -12,7 +29,8 @@ struct cacheline {
 };
 
  cacheline cache[icache_banks][icache_sets][icache_ways];
- char lru[icache_sets]; //stores the least recently used way for each set
+
+ char lru[icache_banks][icache_sets]; //stores the least recently used way for each set for each bank
 
  icache(){
     for(int i = 0; i < icache_banks; i++){
@@ -23,7 +41,7 @@ struct cacheline {
         }
     }
  }
-cacheline icache_access(int addr){
+cacheline* icache_access(int addr){
     int bank_mask = (1 << icache_banks) - 1;
     int set_mask = (1 << icache_sets) - 1;
 
@@ -31,12 +49,22 @@ cacheline icache_access(int addr){
     int set = (addr >> (std::log2(icache_sets))) & set_mask;
 
     int tag = (addr >> 8) & 0x007F; //not sure if this can change
+    return cache[bank][set]; //returns the ways you need to search
+}
+
+ cache_lookup_result try_hit(int addr){
+    cacheline* set = icache_access(addr);
+    int tag = tlb_access(addr);
+    if(tag == -1){
+        return {{}, tlb_miss}; //return instantly if there's no tlb entry
+    }
     for(int i = 0; i < icache_ways; i++){
-        if((tag == cache[bank][set][i].tag) && cache[bank][set][i].valid){
-            return cache[bank][set][i]; //cache hit
+        if(set[i].valid && (set[i].tag == tag)){
+            return {set[i], none}; //this is the line you need to put into the ibuffer
         }
     }
-    return std::nullopt;   //cache miss
+    mshr_preinserter(EIP, icache);
+    return {{}, tag_miss};
 }
 
 
